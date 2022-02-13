96bc0ccbc90562476b59a835df8b4c5d2320dc13
Straxan <jacobstrachan99@gmail.com>
2022-02-13 22:06:51 +0000

    Added game listing file. For use on main page.

---
+++ view/listing.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+    <!--
+        This is adapted from code from my block2. It happens to match a design shown in the wireframes pretty closely.
+        (Slide 12 on powerpoint)
+    -->
+    </head>
+    <body>
+        <?php
+            // Variables will need to be changed 
+            echo '
+                <div class="card container justify-content-center p-4">
+                    <div class="row">
+                        <div class="col-4">
+                            <div><img class="shadow-sm w-75 rounded" src="../image/'. $basket[0]->image .'"/></div> 
+                        </div>
+                        <div class="col-8">
+                            <div class="row">
+                                <div class="col-6">
+                                    <h1 class="font-weight-bold">'. $basket[0]->name .'</h1>
+                                </div>
+                                <div class="col-6">
+                                    <a href="../controller/emptyBasket.php" class="btn btn-outline-success"><i class="bi-x"></i> Remove from Basket</a>
+                                </div>
+                                <div class="col-6">
+                                    <h3>£'. $basket[0]->price .'</h3>
+                                </div>
+                                <div class="col-6">
+                                    <a href="details.php" class="btn btn-lg btn-outline-success">Purchase</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>'
+        ?>
+    </body>
+</html>
