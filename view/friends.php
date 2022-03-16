1ff88631d4a0d97b54347080ee5e3c29a96c2efe
dtimoshenko99 <91141246+dtimoshenko99@users.noreply.github.com>
2022-03-16 15:11:56 +0000

    fixed style file pos

--- view/friends.php
+++ view/friends.php
@@ -18,7 +18,7 @@ $userID = $_SESSION['uID'];
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles/styles.css">
     <title>Friends</title>
 </head>
 
