2d6f1bdcee20966334d6b2738b5d5ae3edb4cb84
dtimoshenko99 <91141246+dtimoshenko99@users.noreply.github.com>
2022-03-18 11:16:01 +0000

    Added page link on Friends button in dropdown menu

--- view/header.php
+++ view/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,76 +14,79 @@
     <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
     <script src="../dist/app.js"></script>
 </head>
+
 <body>
-<nav>
-    <div>
+    <nav>
         <div>
-            <img class="brand_icon" src="image/TPCG_logo_no_text.png"/>
-            <H2>TPCG</H2>
-        </div>
-        <div>
-            <ul>
-                <li><a href="#">Store</a></li>
-                <li><a href="#">Contribute</a></li>
-                <li><a href="#">Community</a></li>
-                <li><a href="#">About</a></li>
-            </ul>
-        </div>
-        <div>
-            <ul>
-                <li><a href="#">Login</a></li>
-                <li><a href="#">Register</a></li>
-            </ul>
-        </div>
-    </div>
-    <div>
-        <div>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Categories</a></li>
-                <li><a href="#">Recently Added</a></li>
-            </ul>
-        </div>
-        <div>
-            <input type="search"/>
-        </div>
-    </div>
-</nav>
-
-
-
-<nav class="mdc-top-app-bar mdc-top-app-bar--dense navbar navbar-default navbar-expand-lg navbar-light">
-    <div class="navbar-header">
-        <img width="48" height="48" src="../image/TPCG_logo_no_text.png" class="mdc-top-app-bar__navigation-icon brand_icon"/>
-        <span class="mdc-top-app-bar__title mdc-top-app-bar__navigation-icon brand_icon" href="../view/index.php">TPCG</span>
-        <div class="mdc-top-app-bar__section">
-            <div id="navbarCollapse">
-                <a class="top-app-bar-mainLink" href="#">Store</a>
-                <a class="top-app-bar-mainLink" href="contribute.php">Contribute</a>
-                <a class="top-app-bar-mainLink" href="community.php">Community</a>
-                <a class="top-app-bar-mainLink" href="about.php">About</a>
+            <div>
+                <img class="brand_icon" src="image/TPCG_logo_no_text.png" />
+                <H2>TPCG</H2>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="#">Store</a></li>
+                    <li><a href="#">Contribute</a></li>
+                    <li><a href="#">Community</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#">Register</a></li>
+                </ul>
             </div>
         </div>
-        <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
-            <span class="navbar-toggler-icon"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <div id="navbarCollapse" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li class="dropdown">
-                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Store <b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="search.php">Categories</a></li>
-                        <li><a href="#">Recently Added</a></li>
-                    </ul>
-                </li>
-                <li><a href="contribute.php">Contribute</a></li>
-                <li><a href="community.php">Community</a></li>
-                <li><a href="about.php">About</a></li>
-            </ul>
-            <?php
+        <div>
+            <div>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Categories</a></li>
+                    <li><a href="#">Recently Added</a></li>
+                </ul>
+            </div>
+            <div>
+                <input type="search" />
+            </div>
+        </div>
+    </nav>
+
+
+
+    <nav class="mdc-top-app-bar mdc-top-app-bar--dense navbar navbar-default navbar-expand-lg navbar-light">
+        <div class="navbar-header">
+            <img width="48" height="48" src="../image/TPCG_logo_no_text.png"
+                class="mdc-top-app-bar__navigation-icon brand_icon" />
+            <span class="mdc-top-app-bar__title mdc-top-app-bar__navigation-icon brand_icon"
+                href="../view/index.php">TPCG</span>
+            <div class="mdc-top-app-bar__section">
+                <div id="navbarCollapse">
+                    <a class="top-app-bar-mainLink" href="#">Store</a>
+                    <a class="top-app-bar-mainLink" href="contribute.php">Contribute</a>
+                    <a class="top-app-bar-mainLink" href="community.php">Community</a>
+                    <a class="top-app-bar-mainLink" href="about.php">About</a>
+                </div>
+            </div>
+            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                <span class="navbar-toggler-icon"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <div id="navbarCollapse" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="dropdown">
+                        <a data-toggle="dropdown" class="dropdown-toggle" href="#">Store <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="search.php">Categories</a></li>
+                            <li><a href="#">Recently Added</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="contribute.php">Contribute</a></li>
+                    <li><a href="community.php">Community</a></li>
+                    <li><a href="about.php">About</a></li>
+                </ul>
+                <?php
 				require_once('config.php');
 				session_start();
 				
@@ -151,7 +155,7 @@
                     echo '<li><a href="#">Account Balance: £9,999</a></li>';
                     echo '<li><a href="account.php">Account Settings</a></li>';
                     echo '<li><a href="#">Key Inventory</a></li>';
-                    echo '<li><a href="#">Friends</a></li>';
+                    echo '<li><a href="friends.php">Friends</a></li>';
 
                     if ($_SESSION['admin']) {
                         echo '<li><a href="#">Admin Panel</a></li>';
@@ -164,8 +168,7 @@
                 echo '</li>';
                 echo '</ul>';
             ?>
+            </div>
         </div>
-    </div>
 
-</nav>
-
+    </nav>
