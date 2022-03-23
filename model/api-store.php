33fa9a7b9442e902cca214fb4d2ce22f53663a89
Straxan <jacobstrachan99@gmail.com>
2022-03-23 19:30:00 +0000

    Basic implementatiomn of basket page

--- model/api-store.php
+++ model/api-store.php
@@ -5,22 +5,23 @@
 	// function to return the details of the game in the user's basket
 	function getBasket()
 	{
-		$conn = getDatabaseConnection();
 		$id = $_SESSION['uID'];
+		$conn = getDatabaseConnection();
 		// Get IDs of user, key, and app, as well as the store used, game name, and the key itself
-		$sql = "SELECT basket.userID, basket.keyID, gameKeys.store, gameKeys.gameKey, steam.name, steam.appid FROM ((basket INNER JOIN gameKeys ON gameKeys.id = basket.keyID) INNER JOIN steam ON steam.appid = gameKeys.appID) WHERE basket.userID = $id";
+		$sql = "SELECT basket.userID, basket.keyID, gameKeys.store, gameKeys.gameKey, steam.name, steam.appid FROM ((basket INNER JOIN gameKeys ON gameKeys.id = basket.keyID) INNER JOIN steam ON steam.appid = gameKeys.appID) WHERE basket.userID = " . $id;
+		//$sql = "SELECT * FROM basket WHERE userID = " . $id;
 		$result = mysqli_query($conn, $sql);
 		while($r = mysqli_fetch_assoc($result)) {
     		$rows[] = $r;
 		}
 
-		/*for ($i=0;$i<sizeof($rows);$i++){
+		for ($i=0;$i<sizeof($rows);$i++){
 			$appID = $rows[$i]["appid"];
 			$url = "https://store.steampowered.com/api/appdetails?appids=" . $appID; // Get info for current game
 			$apidata = json_decode(file_get_contents($url), true);
-			$cost = ceil($apidata[$appID]["data"]["price_overview"]["final"] / 100 ) * 100;
+			$cost = (ceil($apidata[$appID]["data"]["price_overview"]["final"] / 100 )) * 100;
 			$rows[$i]["cost"] = $cost; // Add cost field to array
-		}*/
+		}
 
 		return $rows;
 	}
---
+++ view/basket.php
@@ -0,0 +1,92 @@
+<?php
+	//include('header.php');
+    session_start();
+	include("../model/api-store.php");
+	include("../model/getGames.php");
+
+	// Message code is set to be used later
+    // Zero means default message
+    $msgType = 0;
+    $code = $_GET['msg'];  
+    $msgType += $code;
+?>
+<!DOCTYPE html>
+<head>
+	<!-- The site uses Bootstrap v5 Framework-->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+	</script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+</head>
+
+<body>
+	<!-- overall container for page -->
+	<div class="container">
+		<!--<div class="col-sm-12 mb-2">
+			<div class="card">
+				<div class="card-body">
+					<a href="index.php" class="btn btn-outline-success">Return to Store Page</a>
+				</div>
+			</div>
+		</div>-->
+
+		<div class="card container justify-content-center p-4">
+					
+			<?php
+				
+				$basket = getBasket();
+
+				//var_dump($basket);
+
+				if (sizeof($basket) < 1){
+					switch ($msgType){
+						case 0:
+							echo '<h2 class="card-title text-center">Your basket is empty.</h2>';
+							var_dump($basket);
+							break;
+						case 1:
+							echo '<h2 class="card-title text-center">Purchase was successful!</h2>'; 
+							break;
+						default:
+							echo '<h2 class="card-title text-center">Unknown error.</h2>';
+					}				
+				}else{
+					for ($i=0;$i<sizeof($basket);$i++){
+
+						$cached = checkedGameCached($basket[$i]["appid"])[0];
+						$img = $cached["img"];
+
+						echo '
+						<div class="row">
+							<div class="col-4">
+								<div><img class="shadow-sm w-75 rounded" src="'. $img .'"/></div>
+							</div>
+							<div class="col-8">
+								<div class="row">
+									<div class="col-6">
+										<h1 class="font-weight-bold">'. $basket[$i]["name"] .'</h1>
+									</div>
+									<div class="col-6">
+										<a href="../controller/emptyBasket.php" class="btn btn-outline-success"><i class="bi-x"></i> Remove from Basket</a>
+									</div>
+									<div class="col-6">
+										<h3>'. $basket[$i]["cost"] .' Credits</h3>
+									</div>
+									<div class="col-6">
+										<a href="details.php" class="btn btn-lg btn-outline-success">Purchase</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						';
+					}
+				}
+			?>
+		</div>
+	</div>
+
+</body>
+
+</html>
