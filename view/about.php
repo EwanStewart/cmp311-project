706db93f1e1d73051c362a9ff4a12047b127e7e2
LaserGuidedMelon <1903531@uad.ac.uk>
2022-04-16 03:05:20 +0100

    rebuilt about page and added "who we are" section

--- view/about.php
+++ view/about.php
@@ -2,44 +2,50 @@
     session_start();
     ?>
 <div class="container mdc-top-app-bar--prominent-fixed-adjust">
-    <div class="row justify-content-center">
-        <div class="col-12 col-lg-6">
-            <div class="card-wrapper">
-                <div class="card-box align-left">
-                    <h4 class="card-title">
-                        <strong> What is TPCG? </strong>
-                    </h4>
-                    <p>
-                        Enter text.</p>
+    
+    <div class="row">
+
+        <div class="col-lg-12 justify-content-center">
+            <center>
+
+                <div class="card" style="display: flex; justify-content: center;">
+                    <img class="card-img" src="../image/TPCG_logo.png" style="max-height: 256px; max-width: 256px; margin-left: auto; margin-right: auto; margin-top: 15px;">
+
+                    <div class="card-body" style="padding-top: 0px; padding-bottom: 0px;">
+                        
+                        <h2 class="card-title">Who are we?</h2>
+
+                        <hr>
+                        
+                        <p class="card-text">TPCG, or Tayside PC Gamers, are a group of students from Abertay University.</p>
+
+                        <p class="card-text">Like many PC gamers we use bundle services, such as Humble Bundle, for game keys.</p>
+
+                        <p class="card-text">And like most who use these services, we have an abundance of keys for games that don't appeal to us.</p>
+
+                        <p class="card-text">So we had an idea...</p>
+
+                        <p class="card-text">Instead of trading keys between ourselves and others, on forums and groups.</p>
+
+                        <p class="card-text">A platform should be created.</p>
+
+                        <p class="card-text">One that offers a safe place for those interested.</p>
+
+                        <p class="card-text">With measures in place to ensure a fair system.</p>
+
+                        <p class="card-text" style="margin-bottom: 15px;">Where members can easily trade their spare game keys with each.</p>
+
+                    </div>
                 </div>
-            </div>
+
+            </center>
         </div>
-        <div class="col-12 col-lg-6">
-            <div class="item">
-                <div class="text-box">
-                    <h4 class="icon-title">
-                        <strong>Enter Text.</strong>
-                    </h4>
-                    <h5 class="icon-text">Enter Text.</h5>
-                </div>
-            </div>
-            <div class="item">
-                <div class="text-box">
-                    <h4 class="icon-title">
-                        <strong>Enter Text.</strong>
-                    </h4>
-                    <h5 class="icon-text">Enter Text.</h5>
-                </div>
-            </div>
-            <div class="item">
-                <div class="text-box">
-                    <h4 class="icon-title">
-                        <strong>Enter Text.</strong>
-                    </h4>
-                    <h5 class="icon-text">Enter Text.</h5>
-                </div>
-            </div>
-        </div>
+
+        
+                
     </div>
+
+
+
 </div>
 <?php include('footer.php');
