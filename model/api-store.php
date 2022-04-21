dc8d1775f61761a17321182ba0db1a250579692a
Straxan <jacobstrachan99@gmail.com>
2022-04-21 22:23:57 +0100

    Basket purchasing finally functional

--- model/api-store.php
+++ model/api-store.php
@@ -103,42 +103,43 @@
 		return $totalCost;
 	}
 
-	function purchaseBasket()
-	{
-		$conn = getDatabaseConnection();
-		$id = $_SESSION['uID'];
-		$totalCost = totalBasketCost();
+	// function purchaseBasket()
+	// {
+	// 	$conn = getDatabaseConnection();
+	// 	$id = $_SESSION['uID'];
+	// 	$totalCost = totalBasketCost();
 
-		$sql = "SELECT credit FROM cmp311user WHERE id = $id";
-		$result = mysqli_query($conn, $sql);
-		$balance = $result[0]["credit"];
+	// 	$sql = "SELECT credit FROM cmp311user WHERE id = $id";
+	// 	$result = mysqli_query($conn, $sql);
+	// 	$balance = $result[0]["credit"];
 
-		$newTotal = $balance - $totalCost;
+	// 	$newTotal = $balance - $totalCost;
 		
-		if ($balance < $totalCost){
-			return "You do not have enough Credits for this purchase.";
-		}else{	
-			$basket = getBasket($id);
-			for ($i=0;$i<sizeof($basket);$i++){
-				$keyid = $basket[$i]["keyID"];
-				$cost = $basket[$i]["cost"];
-				// Mark Keys as owned by user who purchased
-				$sql = "UPDATE gameKeys SET purchasedBy = $id WHERE id = $keyid";
-				$result = mysqli_query($conn, $sql);
-				// Make note of transaction in log
-				$sql = "INSERT INTO keyTransactions (userID, keyID, cost) VALUES ($id, $keyid, $cost)";
-			}
-			// Empty basket
-			$sql = "DELETE FROM basket WHERE userID = $id";
-			$result = mysqli_query($conn, $sql);
-			// Take credits from user
-			$sql = "UPDATE cmp311user SET credit = $newTotal WHERE id = $id";
-			$result = mysqli_query($conn, $sql);
+	// 	if ($balance < $totalCost){
+	// 		return "You do not have enough Credits for this purchase.";
+	// 	}else{	
+	// 		$basket = getBasket($id);
+	// 		for ($i=0;$i<sizeof($basket);$i++){
+	// 			$keyid = $basket[$i]["keyID"];
+	// 			$cost = $basket[$i]["cost"];
+	// 			// Mark Keys as owned by user who purchased
+	// 			$sql = "UPDATE gameKeys SET purchasedBy = $id WHERE id = $keyid";
+	// 			$result = mysqli_query($conn, $sql);
+	// 			// Make note of transaction in log
+	// 			$sql = "INSERT INTO keyTransactions (userID, keyID, cost) VALUES ($id, $keyid, $cost)";
+	// 			$result = mysqli_query($conn, $sql);
+	// 		}
+	// 		// Empty basket
+	// 		$sql = "DELETE FROM basket WHERE userID = $id";
+	// 		$result = mysqli_query($conn, $sql);
+	// 		// Take credits from user
+	// 		$sql = "UPDATE cmp311user SET credit = $newTotal WHERE id = $id";
+	// 		$result = mysqli_query($conn, $sql);
 
-			$msg = "Keys purchased successfully.";
-			return $msg;
-		}
-	}
+	// 		$msg = "Keys purchased successfully.";
+	// 		return $msg;
+	// 	}
+	// }
 
 	function checkValidSubscription(){
 		//	function to check for valid subscription
