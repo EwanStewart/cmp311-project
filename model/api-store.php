2d5a0524237bdabf9319e461a5b86ac782a35d18
Straxan <jacobstrachan99@gmail.com>
2022-04-24 11:15:48 +0100

    Removed error reporting in api-store

--- model/api-store.php
+++ model/api-store.php
@@ -2,9 +2,9 @@
 	// Connect to database
 	include_once("../controller/connection.php");
 
-	ini_set('display_errors', '1');
-    ini_set('display_startup_errors', '1');
-    error_reporting(E_ALL);
+	// ini_set('display_errors', '1');
+    // ini_set('display_startup_errors', '1');
+    // error_reporting(E_ALL);
 	
 	// function to return the details of the game in the user's basket
 	function getBasket()
