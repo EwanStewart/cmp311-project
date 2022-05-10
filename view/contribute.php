10241949d960db129354b13953caf5bf5dde6d96
Ewan <ewanstewart04@outlook.com>
2022-05-10 10:46:47 +0100

    final

--- view/contribute.php
+++ view/contribute.php
@@ -19,6 +19,7 @@
         <?php
     }
 ?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script>
     //do something
     jQuery(document).ready(function(){
